f3297ddfda29180b8c64b7a2e1d9e1e9d233512c
Nurik <tanatov1989@mail.ru>
2017-12-07 17:51:15 +0600

    Добавьте файлы проекта.

---
+++ Logic/TV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    interface TV:Switcher,Locker
+    { 
+      void SwitchTheChannelAhead();
+      void SwitchTheChannelBack();
+      void IncreaseTheSound();
+      void DecreaseTheSound();
+
+    }
+}
---
+++ Logic/WashingMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    interface WashingMachine:Switcher
+    {
+        void ChooseTheTypeOfWashing();
+        void ChooseTheDurationOfWashing();
+        void PressClose();
+    }
+}
